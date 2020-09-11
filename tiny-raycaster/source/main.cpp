e87df0c1e3ef2ce564ab8d6291f7e3886d074872
Joshua Robertson <jrobertson774@yahoo.com>
2020-09-11 12:33:43 +0100

    [tiny-raycaster] Wrapped some data into a reusable Image type.

--- tiny-raycaster/source/main.cpp
+++ tiny-raycaster/source/main.cpp
@@ -14,6 +14,12 @@ typedef  int32_t s32;
 static constexpr int WIN_W = 512;
 static constexpr int WIN_H = 512;
 
+struct Image
+{
+    std::vector<u32> pixels;
+    int w, h;
+};
+
 static void unpack_color (u32 color, u8& r, u8& g, u8& b, u8& a)
 {
     r = (color >>  0) & 0xFF;
@@ -27,17 +33,18 @@ static u32 pack_color (u8 r, u8 g, u8 b, u8 a = 0xFF)
     return ((a << 24) + (b << 16) + (g << 8) + (r));
 }
 
-static void save_ppm (const std::string file_name, const std::vector<u32>& pixels, int w, int h)
+static void save_ppm (const std::string file_name, const Image& image)
 {
-    assert((w*h) == pixels.size());
+    assert((image.w*image.h) == image.pixels.size());
 
     std::ofstream file(file_name, std::ios::binary);
     if (file.is_open())
     {
-        file << "P6\n" << w << " " << h << "\n255\n";
-        for (int i=0; i<(w*h); ++i) {
+        file << "P6\n" << image.w << " " << image.h << "\n255\n";
+        for (int i=0; i<image.pixels.size(); ++i)
+        {
             u8 r,g,b,a;
-            unpack_color(pixels[i], r,g,b,a);
+            unpack_color(image.pixels[i], r,g,b,a);
             file << static_cast<u8>(r) << static_cast<u8>(g) << static_cast<u8>(b);
         }
         file.close();
@@ -46,19 +53,26 @@ static void save_ppm (const std::string file_name, const std::vector<u32>& pixel
 
 int main (int argc, char** argv)
 {
-    std::vector<u32> framebuffer(WIN_W*WIN_H, 0x000000FF); // Fill the framebuffer with red.
+    Image framebuffer;
 
-    for (int iy=0; iy<WIN_H; ++iy) {
-        for (int ix=0; ix<WIN_W; ++ix) {
-            u8 r = (u8)(0xFF * iy / WIN_H);
-            u8 g = (u8)(0xFF * ix / WIN_W);
+    framebuffer.pixels.resize(WIN_W*WIN_H, 0x000000FF); // Fill the framebuffer with red.
+
+    framebuffer.w = WIN_W;
+    framebuffer.h = WIN_H;
+
+    for (int iy=0; iy<framebuffer.h; ++iy)
+    {
+        for (int ix=0; ix<framebuffer.w; ++ix)
+        {
+            u8 r = (u8)(0xFF * iy / framebuffer.h);
+            u8 g = (u8)(0xFF * ix / framebuffer.w);
             u8 b = (u8)(0x00);
 
-            framebuffer[iy * WIN_W + ix] = pack_color(r,g,b);
+            framebuffer.pixels[iy * framebuffer.w + ix] = pack_color(r,g,b);
         }
     }
 
-    save_ppm("output.ppm", framebuffer, WIN_W,WIN_H);
+    save_ppm("output.ppm", framebuffer);
 
     return 0;
 }
