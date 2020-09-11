2791e703beee7388e83e4175a63c2e7ea28fc878
Joshua Robertson <jrobertson774@yahoo.com>
2020-09-11 12:41:41 +0100

    [tiny-raycaster] Added 2D topdown map drawing.

--- tiny-raycaster/source/main.cpp
+++ tiny-raycaster/source/main.cpp
@@ -13,6 +13,26 @@ typedef  int32_t s32;
 
 static constexpr int WIN_W = 512;
 static constexpr int WIN_H = 512;
+static constexpr int MAP_W =  16;
+static constexpr int MAP_H =  16;
+
+static constexpr const char MAP[] =
+"0000222222220000"
+"1              0"
+"1      11111   0"
+"1     0        0"
+"0     0  1110000"
+"0     3        0"
+"0   10000      0"
+"0   0   11100  0"
+"0   0   0      0"
+"0   0   1  00000"
+"0       1      0"
+"2       1      0"
+"0       0      0"
+"0 0000000      0"
+"0              0"
+"0002222222200000";
 
 struct Image
 {
@@ -31,6 +51,21 @@ static void unpack_color (u32 color, u8& r, u8& g, u8& b, u8& a)
 static u32 pack_color (u8 r, u8 g, u8 b, u8 a = 0xFF)
 {
     return ((a << 24) + (b << 16) + (g << 8) + (r));
+}
+
+static void draw_rect (Image& image, int x, int y, int w, int h, u32 color)
+{
+    assert((image.w*image.h) == image.pixels.size());
+
+    for (int ix=0; ix<w; ++ix)
+    {
+        for (int iy=0; iy<h; ++iy)
+        {
+            int cx = x+ix, cy = y+iy;
+            assert((cx < image.w) && (cy < image.h));
+            image.pixels[cy * image.w + cx] = color;
+        }
+    }
 }
 
 static void save_ppm (const std::string file_name, const Image& image)
@@ -53,6 +88,8 @@ static void save_ppm (const std::string file_name, const Image& image)
 
 int main (int argc, char** argv)
 {
+    assert(sizeof(MAP) == ((MAP_W*MAP_H)+1)); // +1 for the null-terminator.
+
     Image framebuffer;
 
     framebuffer.pixels.resize(WIN_W*WIN_H, 0x000000FF); // Fill the framebuffer with red.
@@ -60,6 +97,7 @@ int main (int argc, char** argv)
     framebuffer.w = WIN_W;
     framebuffer.h = WIN_H;
 
+    // Draw a smooth gradient across the whole of the framebuffer.
     for (int iy=0; iy<framebuffer.h; ++iy)
     {
         for (int ix=0; ix<framebuffer.w; ++ix)
@@ -69,6 +107,20 @@ int main (int argc, char** argv)
             u8 b = (u8)(0x00);
 
             framebuffer.pixels[iy * framebuffer.w + ix] = pack_color(r,g,b);
+        }
+    }
+
+    // Draw tiles for each of the filled in spaces on the map.
+    constexpr int RECT_W = WIN_W / MAP_W;
+    constexpr int RECT_H = WIN_H / MAP_H;
+
+    for (int iy=0; iy<MAP_H; ++iy)
+    {
+        for (int ix=0; ix<MAP_W; ++ix)
+        {
+            if (MAP[iy * MAP_W + ix] == ' ') continue;
+            int rx = ix*RECT_W, ry = iy*RECT_H;
+            draw_rect(framebuffer, rx, ry, RECT_W, RECT_H, pack_color(0x00,0xFF,0xFF));
         }
     }
 
