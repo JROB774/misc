8a47239e9008bf78bd368911b4ec783ad08e8ad7
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-16 19:49:44 +0000

    Made the test application produce an actual window.

--- sdl2-with-osx/main.cpp
+++ sdl2-with-osx/main.cpp
@@ -1,8 +1,34 @@
-#include <stdio.h>
 #include <SDL.h>
 
 int main (int argc, char** argv)
 {
-    printf("Hello, SDL2!\n");
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Test", SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, 1280,720, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+            {
+                running = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255,0,0,255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    SDL_Quit();
+
     return 0;
 }
