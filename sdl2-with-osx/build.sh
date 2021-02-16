f624afb367e612c19b39e4cace56979184ca4056
Joshua Robertson <joshrobertson774@gmail.com>
2021-02-16 19:44:07 +0000

    Building a test application linking with SDL2 on MacOS.

---
+++ sdl2-with-osx/build.sh
@@ -0,0 +1,1 @@
+g++ main.cpp -o main -I ./SDL2/SDL2.framework/Headers -F ./SDL2 -framework SDL2 -rpath ./SDL2
---
+++ sdl2-with-osx/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <SDL.h>
+
+int main (int argc, char** argv)
+{
+    printf("Hello, SDL2!\n");
+    return 0;
+}
